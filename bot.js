83a72b475d59ee1a1f36c500969a67f9b8a1a5b6
cromaky96 <cromaky96@gmail.com>
2025-10-23 02:51:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,19 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-// Вставьте сюда ваш токен
 const token = '8324358831:AAGNLTEteXzAmdyv58U0e4OmTEo2tFZfYQA';
-
-// Создаем бота
 const bot = new TelegramBot(token, { polling: true });
 
-// Обработка команды /start
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   const options = {
     reply_markup: {
       inline_keyboard: [
-        [{ text: 'Открыть сайт', url: 'https://cromaky96.github.io/miniapp/' }]
+        [{ 
+          text: 'Открыть MiniApp', 
+          web_app: { url: 'https://cromaky96.github.io/miniapp/' } 
+        }]
       ]
     }
   };
-  bot.sendMessage(chatId, 'Нажмите кнопку ниже, чтобы открыть сайт:', options);
+  bot.sendMessage(chatId, 'Нажмите кнопку ниже, чтобы открыть MiniApp внутри Telegram:', options);
 });
